480712bb3b0977ed5e6bd8aa688c81b9c1b93f69
hakim671 <boyzodahakim2008@gmail.com>
2025-03-04 20:30:20 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+df = pd.read_excel('final_prj.xlsx')
